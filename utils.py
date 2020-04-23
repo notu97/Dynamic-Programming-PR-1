0427b4c12086f430928b374661c0c6f054198e3f
Shiladitya Biswas <shiladitya97@outlook.com>
2020-04-23 13:20:11 -0700

    Final Commit

--- utils.py
+++ utils.py
@@ -133,8 +133,9 @@ def draw_gif_from_seq(seq, env, path='./gif/doorkey.gif'):
         img = env.render('rgb_array', tile_size=32)
         writer.append_data(img)
         for act in seq:
-            img = env.render('rgb_array', tile_size=32)
             step(env, act)
+            img = env.render('rgb_array', tile_size=32)
+            # step(env, act)
             writer.append_data(img)
     print('GIF is written to {}'.format(path))
     return
